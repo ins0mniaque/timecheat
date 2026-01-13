d6e9dba99d0498533f80714d1bd03787734421db
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2026-01-12 21:15:44 -0500

    Add GitLab merge request cache

--- GitLab.cs
+++ GitLab.cs
@@ -16,14 +16,15 @@ internal static partial class GitLab
 
         var mrClient = client.GetMergeRequest(projectId);
         var commitClient = client.GetCommits(projectId);
-
-        var mergeRequests = mrClient.Get(new MergeRequestQuery
+        var mergeRequests = mrClient.Get(new()
         {
             UpdatedAfter = start,
             UpdatedBefore = end,
             OrderBy = "updated_at",
             Sort = "asc"
-        }).Where(mr => mr.State is "opened" or "merged");
+        });
+
+        mergeRequests = mergeRequests.Where(mr => mr.State is "opened" or "merged");
 
         if (!string.IsNullOrEmpty(author))
         {
@@ -36,14 +37,12 @@ internal static partial class GitLab
 
         foreach (var mr in mergeRequests)
         {
-            var issueIds = ExtractIssueIds(
-                $"{mr.SourceBranch}\n{mr.Title}\n{mr.Description}");
+            var issueIds = ExtractIssueIds($"{mr.SourceBranch}\n{mr.Title}\n{mr.Description}");
+            var commitIds = cache.GetMergeRequestCommitIds(mrClient, mr);
 
-            var mrCommits = mrClient.Commits(mr.Iid);
-
-            foreach (var commit in mrCommits.All)
+            foreach (var commitId in commitIds)
             {
-                var detailedCommit = cache.GetCommit(commitClient, commit.ShortId);
+                var detailedCommit = cache.GetCommit(commitClient, commitId);
 
                 if (issueIds.Count is 0)
                     result.Add(CreateCommitInfo(detailedCommit, null));
@@ -79,19 +78,15 @@ internal static partial class GitLab
             .Distinct()];
     }
 
-    [GeneratedRegex(@"([A-Z][A-Z0-9]+-\d+)", RegexOptions.IgnoreCase)]
-    private static partial Regex IssueIdRegex();
-
     public static string DetectIssuePatternFromMergeRequests(this GitLabClient client, long projectId)
     {
         var mrClient = client.GetMergeRequest(projectId);
-
-        var mrs = mrClient.Get(new MergeRequestQuery
+        var mergeRequests = mrClient.Get(new MergeRequestQuery
         {
             State = MergeRequestState.merged
         });
 
-        var prefix = mrs.Take(50)
+        var prefix = mergeRequests.Take(50)
             .SelectMany(mr =>
                 IssueIdRegex().Matches(
                     $"{mr.SourceBranch} {mr.Title} {mr.Description}")
@@ -105,12 +100,17 @@ internal static partial class GitLab
             ? $@"{prefix.Key}"
             : @"[A-Z]+";
     }
+
+    [GeneratedRegex(@"([A-Z][A-Z0-9]+-\d+)", RegexOptions.IgnoreCase)]
+    private static partial Regex IssueIdRegex();
 }
 
 internal sealed class GitLabCache
 {
-    private readonly string _cacheDir;
-    private readonly ConcurrentDictionary<string, Commit> _memoryCache = new();
+    private readonly string _commitsCacheDir;
+    private readonly string _mergeRequestsCacheDir;
+    private readonly ConcurrentDictionary<string, Commit> _commitsCache = new();
+    private readonly ConcurrentDictionary<long, List<string>> _mergeRequestsCache = new();
 
     public GitLabCache(string appName)
     {
@@ -118,43 +118,77 @@ internal sealed class GitLabCache
         if (string.IsNullOrEmpty(baseDir))
             baseDir = Directory.GetCurrentDirectory();
 
-        _cacheDir = Path.Combine(baseDir, appName, "commits");
-        Directory.CreateDirectory(_cacheDir);
+        _commitsCacheDir = Path.Combine(baseDir, appName, "commits");
+        _mergeRequestsCacheDir = Path.Combine(baseDir, appName, "merge-requests");
+        Directory.CreateDirectory(_commitsCacheDir);
+        Directory.CreateDirectory(_mergeRequestsCacheDir);
     }
 
-    public Commit GetCommit(ICommitClient commitClient, string commitSha)
+    public Commit GetCommit(ICommitClient commitClient, string commitId)
     {
-        if (_memoryCache.TryGetValue(commitSha, out var cachedCommit))
+        if (_commitsCache.TryGetValue(commitId, out var cachedCommit))
             return cachedCommit;
 
-        var cacheFile = Path.Combine(_cacheDir, $"{commitSha}.json");
+        var cacheFile = Path.Combine(_commitsCacheDir, $"{commitId}.json");
 
         if (File.Exists(cacheFile))
         {
             cachedCommit = JsonSerializer.Deserialize(File.ReadAllText(cacheFile), GitLabContext.Default.Commit)
-                           ?? throw new JsonException($"Failed to deserialize commit {commitSha}");
+                           ?? throw new JsonException($"Failed to deserialize commit {commitId}");
         }
         else
         {
-            cachedCommit = commitClient.GetCommit(commitSha);
+            cachedCommit = commitClient.GetCommit(commitId);
 
             File.WriteAllText(cacheFile, JsonSerializer.Serialize(cachedCommit, GitLabContext.Default.Commit));
         }
 
-        _memoryCache[commitSha] = cachedCommit;
+        _commitsCache[commitId] = cachedCommit;
         return cachedCommit;
+    }
+
+    public IReadOnlyList<string> GetMergeRequestCommitIds(IMergeRequestClient mrClient, MergeRequest mergeRequest)
+    {
+        if (mergeRequest.State is not "merged")
+            return mrClient.Commits(mergeRequest.Iid).All.Select(c => c.ShortId).ToList();
+
+        if (_mergeRequestsCache.TryGetValue(mergeRequest.Iid, out var cachedIds))
+            return cachedIds;
+
+        var cacheFile = Path.Combine(_mergeRequestsCacheDir, $"{mergeRequest.Iid}.json");
+
+        if (File.Exists(cacheFile))
+        {
+            cachedIds = JsonSerializer.Deserialize(File.ReadAllText(cacheFile), GitLabContext.Default.ListString) ??
+                        throw new JsonException($"Failed to deserialize MR {mergeRequest.Iid} commit IDs");
+        }
+        else
+        {
+            cachedIds = mrClient.Commits(mergeRequest.Iid).All.Select(c => c.ShortId).ToList();
+
+            File.WriteAllText(cacheFile, JsonSerializer.Serialize(cachedIds, GitLabContext.Default.ListString));
+        }
+
+        _mergeRequestsCache[mergeRequest.Iid] = cachedIds;
+        return cachedIds;
     }
 
     public void ClearCache()
     {
-        _memoryCache.Clear();
+        _commitsCache.Clear();
+        _mergeRequestsCache.Clear();
 
-        if (Directory.Exists(_cacheDir))
-            Directory.Delete(_cacheDir, true);
+        if (Directory.Exists(_commitsCacheDir))
+            Directory.Delete(_commitsCacheDir, true);
 
-        Directory.CreateDirectory(_cacheDir);
+        if (Directory.Exists(_mergeRequestsCacheDir))
+            Directory.Delete(_mergeRequestsCacheDir, true);
+
+        Directory.CreateDirectory(_commitsCacheDir);
+        Directory.CreateDirectory(_mergeRequestsCacheDir);
     }
 }
 
 [JsonSerializable(typeof(Commit))]
+[JsonSerializable(typeof(List<string>))]
 internal sealed partial class GitLabContext : JsonSerializerContext;
